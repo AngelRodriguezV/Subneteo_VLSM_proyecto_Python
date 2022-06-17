d766277d102fb53882a0fc43374de20b1a7868ab
Angel Rodriguez <venegasrv@outlook.com>
2022-06-16 22:38:13 -0500

    Inicialización del paquete

---
+++ subneteo_vlsm/__init__.py
@@ -0,0 +1,6 @@
+__title__ = "Subneteo VLSM"
+__version__ = "0.0.1"
+__author__ = "Angel Rodriguez"
+__emal__ = "venegasrv@outlook.com"
+__description__ = "Paquete para operar redes y subredes"
+__url__ = "https://github.com/AngelRodriguezV/Subneteo_VLSM_proyecto_Python"
